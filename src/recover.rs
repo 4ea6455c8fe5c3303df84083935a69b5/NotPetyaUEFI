ac5e7659e26da93676db2e1e137e3b54c1ad589c
4ea6455c8fe5c3303df84083935a69b5 <lovesgdimalware@gmail.com>
2025-08-17 19:48:54 +0700

    Update recover.rs

--- src/recover.rs
+++ src/recover.rs
@@ -120,9 +120,9 @@ pub fn recover(st: &mut SystemTable<Boot>, key_bytes: &[u8]) -> uefi::Result {
     }
 
     st.stdout().clear()?;
+    st.stdout().set_color(Color::Red, Color::LightGray)?;
     st.stdout().write_str("Decryption success! Please restart or turn off your PC.").unwrap();
     st.stdout().write_str("\nThank you for using NotPetyaAgain. Hope to NOT see you again.").unwrap();
-    st.stdout().set_color(Color::Red, Color::LightGray)?;
     write_var(st, "NotPetyaAgainId", &[]).unwrap();
 
     let windows_image = read_file(st, r"EFI\Microsoft\Boot\bootmgfw.efi.old")?;
